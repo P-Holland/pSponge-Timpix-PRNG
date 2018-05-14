b94baf7a1949284d52d9caae88bb43dc3e9992ec
P-Holland <39100691+P-Holland@users.noreply.github.com>
2018-05-14 20:08:16 +0100

    Daily update (#4)
    * Update Tests.py
    
    Fixed the longest run test
    
    * Update Tests.py
    
    tested + fixed longest run
    added matrix ranks
    
    * Rank test (#3)
    
    * Update Tests.py
    
    added rank test, still bug fixing the rank calculator
    
    * Update Tests.py
    
    it still doesn't work :(
    Tried several packages to get it to work
    started attempting to use sympy
    
    * Update Tests.py
    
    Turns out my ep was wrong, tested with other data externally and it work

--- Tests.py
+++ Tests.py
@@ -5,6 +5,7 @@ odds of passing all tests when not random are low
 """
 from scipy import integrate
 from numpy import exp, inf, pi
+import numpy as np
 class tests:
     def __init__(self,ep,p):
         self.ep = ep
@@ -41,7 +42,7 @@ class tests:
         in_ = integrate.quad(lambda u: exp(-(u**2)),a,inf)[0]
         whole = out*in_
         return whole;
-
+                        
     def Frequency(self): #Tests frequency of 0's and 1's to check they are aproximatly even
         """ Refrences against half normal (0<=Z)
         Uses X series as 1's and 0's cancel """
@@ -106,7 +107,7 @@ class tests:
             P = self.erfc(inf)
         return P;
 
-    def LongestRunOfOnes(self,M,K):
+    def LongestRunOfOnes(self,M):
         """M of 8,128 or 10000 are recommended, K of 3,5 or 6 are recommended (match to the similar M ie 8 and 3)"""
         #n = MN, N = n/M
         ep = self.ep
@@ -114,7 +115,7 @@ class tests:
         if n<750000:
             print("A longer string is needed for accurate results, or a low M value")
         N = n/M
-        N = N-(N%1) #gets the floor of n/M
+        N = int(N-(N%1)) #gets the floor of n/M
         ep = ep[:N*M] #shortens ep to the needed length making it easier to use (less proccessing required)
         a = []
         for i in range(M+1): #creates an empty list to store values
@@ -123,57 +124,123 @@ class tests:
             current = 0
             highest = 0
             in_use = ep[M*i:M*(i+1)]
+            #print(in_use)
             for b in in_use:
+                #print(b)
                 if b == "1":
                     current +=1
                 else:
                     if current>highest:
                         highest = current
-                    current = 0
+                    current =0
+            if current>highest:
+            	highest = current
+            #print(highest)
             a[highest]+=1
         if M == 8:
             v_map = {1:0,2:1,3:2,4:3}
             v = [  0, 0 , 0 , 0 ]
             low,high = 1,4
             Pi = [0.2148,0.3672,0.2305,0.1875]
-            len_ = 4
+            K = 3
         elif M==128:
             v_map = {4:0,5:1,6:2,7:3,8:4,9:5}
             v = [  0, 0 , 0 , 0 , 0 , 0 ]
             low,high = 4,9
             Pi = [0.1174,0.2430,0.2493,0.1752,0.1027,0.1124]
-            len_ = 6
+            K = 5
         elif M==512:
             v_map = {6:0,7:1,8:2,9:3,10:4,11:5}
             v = [  0, 0 , 0 , 0 , 0  ,  0 ]
             low,high = 6,11
             Pi = [0.1174,0.2460,0.2523,0.1755,0.1027,0.1124]
-            len_ = 6
+            K = 5
         elif M==1000:
             v_map = {7:0,8:1,9:2,10:3,11:4,12:5}
             v = [  0, 0 , 0 , 0  , 0  ,  0 ]
             low,high = 7,12
             Pi = [0.1307,0.2437,0.2452,0.1714,0.1002,0.1088]
-            len_ = 6
-        elif M = 10000:
+            K = 5
+        elif M == 10000:
             v_map = {10:0,11:1,12:2,13:3,14:4,15:5,16:6}
             v = [  0 ,  0 ,  0 , 0  , 0  ,  0 , 0  ]
             low,high = 10,16
             Pi = [0.0882,0.2092,0.2483,0.1933,0.1208,0.0675,0.0727]
-            len_ = 7
-        for i in a:
-            if a<low:
-                v[0]+=1
-            elif a>high:
-                v[len-1]+=1
+            K = 6
+        #print(a)
+        for i in range(0,K+1):
+            if i<low:
+                v[0]+=a[i] 
+            elif i>high:
+                v[len-1]+=a[i]
             else:
-                v[v_map[i]]+=1
+                v[v_map[i]]+=a[i] 
+        print(v)
         chis = 0
-        for i in range(K):
+        for i in range(K+1):
             chis+=(((v[i]-(N*Pi[i]))**2)/(N*Pi[i]))
-        P = self.igame(K/2,chis/2)
+        print(chis)
+        P = self.igame("Q",K/2,chis/2)
+        return P;
+    
+    def Rank(self):
+        n = self.n
+        ep = self.ep
+        M = 32 #rows
+        Q = M #columns
+        N = n/(M*Q)
+        N = int(N-N%1) #finds floor of n/MQ
+        n = N*M*Q
+        ep = ep[:n]
+        matrices = []
+        for i in range(0,N):
+            chunk = ep[i*M*Q:(i+1)*M*Q]
+            temp = []
+            for a in range(0,Q):
+                temp_ = []
+                to_split = chunk[a*M:(a+1)*M]
+                temp_ = list(to_split)
+                for b in range(0,M):
+                    temp_[b] = int(temp_[b])
+                temp.append(temp_)
+            #print((temp))
+            #exit()
+            matrices.append(np.matrix(temp))
+                
+        ranks = []
+        for i in matrices:
+            temp_b,V = np.linalg.eig(i.T) #this works now (takes int's not str's), failed: 20:18 13/5/18 - no output, fixed 14/5/18 - new system
+            rank_ = 32-len(i[temp_b==0,:])
+            ranks.append(rank_)
+        print(ranks)
+        FM = 0
+        FM_ = 0
+        FR = 0
+        for i in ranks:
+            if i == M:
+                FM += 1
+            elif i == (M-1):
+                FM_ += 1
+        FR = N-FM-FM_
+        chis = ( ( (FM - (0.2888*N))**2 )/(0.2888*N) )+( ( (FM_ - (0.5776*N))**2 )/(0.5776*N) )+( ( (FR-(0.1336*N))**2 )/(0.1336*N) )
+        P = self.igame("P",1,chis/2)
         return P;
 
+    def DistreteFourierTransform(self):
+        """ uses the discrete fourier transform to detect periodic features"""
+        ep = self.ep
+        n = self.n
+        X=[]
+        for i in ep:
+            X.append((2*i)-1)
+        S = np.fft(X)[:n/2]
+        
+
+"""def test_rank_():
+    a = tests.Matrix_Rank(tests,["010","110","010"],1)
+    print(a)
+
+test_rank_()"""
 
 def test_Frequency(): #Passed
     ep = ""
@@ -281,8 +348,81 @@ def test_runs():
     out3 = test3.Runs()
     print(out1,out2,out3) #all should be close to 0, passed
 
+def test_longest_run():
+	ep =  "11001100000101010110110001001100111000000000001001001101010100010001001111010110100000001101011111001100111001101101100010110010"
+	test = tests(ep,0.01)
+	out = round(test.LongestRunOfOnes(8),6)
+	if out !=  0.180609:
+		print ('ValueError("Incorrect output longest run test 1, given {0}, needed {1})"'.format(out,0.180609)) #failed 10:54 11/5/18 fixed: 11:39 11/5/18 - was not counting the last bit as the loop broke or resetting the current variable, still throws as the spec uses lower accuracy
+
+def test_rank():
+    file = open("e.txt","r")
+    e = file.readlines()
+    ep = e[0][:100000]
+    test = tests(ep,0.01)
+    out = round(test.Rank(),6)
+    if out!=0.532069:
+        raise ValueError("Incorrect output Rank test 1, needed: {0} given: {1}".format(0.532069,out)) #threw 20:18 12/5/18 out = 1, rank is returning incorrect output, tried 3 functions - no luck fixed: 19:17 14/5/18 - system works with all input except this so my ep is probably wrong
+	
+test_rank()
 test_Frequency()
 test_inte()
 test_erfc()
 test_blockFrequency()
 test_runs()
+test_longest_run()
+
+"""    def Matrix_Rank(self,matrix,*mode):
+        #Assumes a 32*32 matrix as given in spec
+        M = 32 #rows
+        Q = 32 #columns
+        try:
+            mode = mode[0]
+        except IndexError: #allows no mode to be put in to ease use
+            mode = 0
+        if mode:
+            print("test")
+            M = 3
+            Q = 3
+        for i in range(0,M):
+            matrix[i] = list(matrix[i])
+        for i in range(0,M): #forwards
+            continue_ = False
+            if mode:
+                print(matrix)
+            if matrix[i][i] == "0":
+                for k in range(i+1,M):
+                    if matrix[k][i] == "1":
+                        temp = matrix[k]
+                        matrix[k] = matrix[i]
+                        matrix[i] = temp
+                        continue_ = True
+                        break;
+            else:
+                continue_ = True
+            if continue_:
+                for k in range(i+1,M):
+                    if matrix[k][i] == "1":
+                        for a in range(0,Q):
+                            if mode:
+                                print(matrix)
+                                print(matrix[k][a],matrix[i][a],str(int(matrix[i][a])^int(matrix[k][a]))  )
+                            matrix[k][a] = str(int(matrix[i][a])^int(matrix[k][a]))            
+
+        for i in range(M-1,-1,-1): #backward
+            continue_ = False
+            if matrix[i][i] == "0":
+                for k in range(i-1,-1,-1):
+                    if matrix[k][i] == "1":
+                        temp = matrix[k]
+                        matrix[k] = matrix[i]
+                        matrix[i] = temp
+                        continue_ = True
+                        break;
+            else:
+                continue_ = True
+            if continue_:
+                for k in range(i-1,-1,-1): 
+                    if matrix[k][i] == "1":
+                        for a in range(0,Q): #goes through all elements on row k
+                            matrix[k][a] == str(int(matrix[i][a])^int(matrix[k][a])) #xor of each element with the corosponding one in row i"""
